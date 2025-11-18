bdb4a5048a1f76b8ac1d106b31b275f221882d56
Alvar Liljeblad <alvar.liljeblad@skola.taby.se>
2025-11-18 10:30:10 +0100

    added function for generating a random item

--- Item.py
+++ Item.py
@@ -1,3 +1,4 @@
+import random
 class Item:
     def __init__(self,name:str,potency:int):
         self.name: str = name
@@ -8,14 +9,26 @@ class Item:
         pass
     def discard(self,char):
         pass
+    def generate():
+        if random.randint(0,1) == 1:
+            return PassiveStrengthItem.generate()
+        else:
+            return HealingItem.generate()
 
 class PassiveStrengthItem(Item):
+    names = ["Amulet of strenth","Ring of strength","Bracer","Strength rune"]
     def gain(self, char):
         char.strength_bonus += self.potency
     def discard(self, char):
         char.strength_bonus -= self.potency
+    def generate():
+        return PassiveStrengthItem(random.choice(PassiveStrengthItem.names),random.randint(1,4))
 
 class HealingItem(Item):
+    names = ["Health potion","Healing draught","Medicine box"]
     def activate(self, char):
         char.hp = min(char.hp+self.potency,char.max_hp)
         char.inventory.remove(self)
+    def generate():
+        return HealingItem(random.choice(HealingItem.names),random.randint(1,4))
+
