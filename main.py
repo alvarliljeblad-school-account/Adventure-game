116f91dced1e44d04acc17d6d36dba5f771c176a
Alvar Liljeblad <alvar.liljeblad@skola.taby.se>
2025-11-18 09:05:24 +0100

    added a main function containing a selection menu

---
+++ main.py
@@ -0,0 +1,25 @@
+import Character
+import Item
+import Room
+def main():
+    player = Character.Character(3,10)
+    while player.hp > 0:
+        print("""What would you like to do?
+              1: See stats || 2: See inventory
+              3: Use item  || 4: Enter a room
+              q: quit game""")
+        selection = input("Selection -> ")
+        if selection == "1":
+            player.display_stats()
+        elif selection == "2":
+            player.display_inventory()
+        elif selection == "3":
+            ...
+        elif selection == "4":
+            ...
+        elif selection == "q":
+            return
+
+
+if __name__ == "__main__":
+    main()
