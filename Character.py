911d00e2dc52594c7064977719238d0482902419
Alvar Liljeblad <alvar.liljeblad@skola.taby.se>
2025-11-19 12:38:00 +0100

    added pauses to the methods for the player

--- Character.py
+++ Character.py
@@ -1,4 +1,5 @@
 class Character:
+    """Class for the player character, containing stats and methods for displaying them"""
     def __init__(self,strength, hp):
         self.strength: int = strength
         self.strength_bonus:int = 0
@@ -8,12 +9,16 @@ class Character:
         self.level:int = 1
         self.damage: int = 3
     def display_stats(self):
+        """Prints the players current stats"""
         print(f"""
                 Stats:
             Hp: {self.hp}/{self.max_hp}
             Level: {self.level}
             Strength: base: {self.strength} + items: {self.strength_bonus} = {self.strength+ self.strength_bonus}""")
+        input("Press enter to continue") # to pause until the player presses enter
     def display_inventory(self):
+        """Prints the constents of the playes inventory"""
         print("Inventory:")
         for i in range(len(self.inventory)):
             print(f"{i}: {self.inventory[i]}")
+        input("Press enter to continue") #Pause until the player presses enter
