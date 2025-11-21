7076b017ca797abdb2d991bcd2bc6bf52ef0645f
Alvar Liljeblad <alvar.liljeblad@skola.taby.se>
2025-11-21 11:51:52 +0100

    bug fix

--- gameInput.py
+++ gameInput.py
@@ -1,4 +1,5 @@
-def get_str_input(possible_inputs:list[str]=[],input_str:str = "->")->str:
+def get_str_input(possible_inputs:list,input_str:str = "->")->str:
+    possible_inputs = map(lambda a: str(a), possible_inputs)
     """Returns an input if it is in list of possible inputs, else it will try again"""
     player_input = ""
     #Keep taking player input until a valid input is obtained
