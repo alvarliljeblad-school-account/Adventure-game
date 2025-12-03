184b79cad17cb67dbe84f851a1b3d6510e0c9b21
Alvar Liljeblad <alvar.liljeblad@skola.taby.se>
2025-12-03 12:58:55 +0100

    added a simpe start function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import Character
+from Character import Character
 import Room
-import gameInput
+from gameInput import get_str_input
 from world import World
-
+MAP_FILE = "floor_layouts.txt"
 def win() -> None:
     """Prints the win text"""
     print("You have won!!")
@@ -13,13 +13,18 @@ def death() -> None:
     print("You died")
     print("Boohoo")
 
-def start() -> None:
-    pass
+def start() -> World:
+    """Start function, contains all setup code executed when the game stars"""
+    print("Welcome to the game")
+    print("What floor would you like to go to")
+    floor_id = get_str_input(["1"])
+    player = Character(4,4)
+    floor = World.open_layout_from_file(MAP_FILE,floor_id,player)
+    return floor
 
 def main() -> None:
     """Main function of the program, contains the mainloop"""
-    floor = World()
-    floor.player = Character.Character(5,3) 
+    floor = start()
     floor.take_turn()
 
 
