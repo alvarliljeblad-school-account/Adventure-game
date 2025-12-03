6843f416afcc78e30fe2985eb79cda75adf27b48
Alvar Liljeblad <alvar.liljeblad@skola.taby.se>
2025-12-03 12:40:20 +0100

    added typing to vectors

--- vector.py
+++ vector.py
@@ -1,8 +1,8 @@
 import math
 class Vec2:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+    def __init__(self,x:int,y:int):
+        self.x:int = x
+        self.y:int = y
     def __add__(self,other):
         return Vec2(self.x+other.x,self.y,other.y)
     def __sub__(self,other):
